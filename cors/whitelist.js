ec882862405870ad5d6b8eaa99ec5336c16a8122
Gustavo-Gugabit <gugabit2810@gmail.com>
2021-08-16 19:12:10 -0300

    update cors

---
+++ cors/whitelist.js
@@ -0,0 +1,11 @@
+export default [
+  "",
+  "http://localhost:8080",
+  "https://localhost:8080",
+  "http://localhost:8081",
+  "https://localhost:8081",
+  "http://localhost:8082",
+  "https://localhost:8082",
+  "http://dashboard.clinic.inf.br",
+  "https://dashboard.clinic.inf.br",
+]
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
+const cors = require('./cors')
 
 require('dotenv/config')
 
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use(morgan('dev'))
-app.use(cors())
+app.use(cors)
 
 app.listen(process.env.PORT, () => console.log(`Everything okay, Running at port: ${process.env.PORT}`))
 
